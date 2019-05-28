2ee4032b307105cecafe08dad3a6fc22c9453e5e
Parneet kaur Jattana <Parneet kaur Jattana@DESKTOP-S01EL89>
2019-05-28 16:35:08 -0400

    last15characters

--- Quiz3_4.java
+++ Quiz3_4.java
@@ -4,9 +4,17 @@ public class Quiz3_4 {
 
 	public static void main(String[] args) {
 	
-		String sentence= "“copyandpastecopyandpastecopyandpastecopyandpastecopyandpastecopyandpastecommitandpushcommitandpushcommitandpushcommitandpushpushcccccommmitttsss”";
+		String sentence= "copyandpastecopyandpastecopyandpastecopyandpastecopyandpastecopyandpastecommitandpushcommitandpushcommitandpushcommitandpushpushcccccommmitttsss";
 		int numCharInSentence=sentence.length();
-		System.out.println("num of charatcters " +numCharInSentence);
+		System.out.println("num of characters " +numCharInSentence);
+		
+		String subString=sentence.substring(sentence.length()-15);
+		System.out.println("Last 15 characters of string are:" +subString);
+		
+		
+		
+		
+		
 
 	}
 
