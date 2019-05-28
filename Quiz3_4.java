2f7a3d7593e5b4dbb216edaebfdd721705f404b6
Parneet kaur Jattana <Parneet kaur Jattana@DESKTOP-S01EL89>
2019-05-28 16:12:55 -0400

    NumberOfCharacters

---
+++ Quiz3_4.java
@@ -0,0 +1,13 @@
+import java.lang.String;
+//Paramjeet Kaur(747429);
+public class Quiz3_4 {
+
+	public static void main(String[] args) {
+	
+		String sentence= "“copyandpastecopyandpastecopyandpastecopyandpastecopyandpastecopyandpastecommitandpushcommitandpushcommitandpushcommitandpushpushcccccommmitttsss”";
+		int numCharInSentence=sentence.length();
+		System.out.println("num of charatcters " +numCharInSentence);
+
+	}
+
+}
